801a9da72dd3c86ce4e4c7c56d00b85db4b083c8
ViperEvo <speed6767@wp.pl>
2018-12-05 21:17:15 +0100

    Potem to pchnę

--- Script.js
+++ Script.js
@@ -1,6 +1,8 @@
 function addNote(){
     document.getElementById('overlay').style.display='block';
+    document.getElementById('inputBox').style.display='block';
 }
 function closeOverlay(){
     document.getElementById('overlay').style.display='none';
+    document.getElementById('inputBox').style.display='none';
 }
