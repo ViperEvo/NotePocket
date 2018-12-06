41069e00f95f4b19b9c92389bb10658f0c6a3a0e
ViperEvo <speed6767@wp.pl>
2018-12-06 18:40:09 +0100

    Powoli XD

--- Script.js
+++ Script.js
@@ -1,3 +1,6 @@
+noteTitle = document.getElementById("noteTitle");
+note = document.getElementById("note");
+
 function addNote(){
     document.getElementById('overlay').style.display='block';
     document.getElementById('inputBox').style.display='block';
@@ -5,4 +8,13 @@ function addNote(){
 function closeOverlay(){
     document.getElementById('overlay').style.display='none';
     document.getElementById('inputBox').style.display='none';
+}
+function addNoteF(){
+    let ndata = { "Title" : noteTitle.value,"Note" : note.value};
+    let noteData = JSON.stringify(ndata);
+    console.log(noteData);
+}
+function removeNoteF(){
+    noteTitle.value = "";
+    note.value = "";
 }
