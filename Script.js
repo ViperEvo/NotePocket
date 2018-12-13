81beaa58c62f4937387b58710bb659e99e303315
ViperEvo <speed6767@wp.pl>
2018-12-13 22:52:30 +0100

    No taka baza do dalszego kminienia

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 noteTitle = document.getElementById("noteTitle");
 note = document.getElementById("note");
-
+let numer = 0;
 function addNote(){
     document.getElementById('overlay').style.display='block';
     document.getElementById('inputBox').style.display='block';
@@ -10,11 +10,17 @@ function closeOverlay(){
     document.getElementById('inputBox').style.display='none';
 }
 function addNoteF(){
-    let ndata = { "Title" : noteTitle.value,"Note" : note.value};
+    let ndata = { "Title" : noteTitle.value,"Note" : note.value, "X" : numer};
     let noteData = JSON.stringify(ndata);
+    localStorage.setItem(numer, noteData);
+    numer += 1;
     console.log(noteData);
+    console.log(numer);
+    console.log(localStorage.getItem('Notka') + "To jest storage");
 }
 function removeNoteF(){
     noteTitle.value = "";
     note.value = "";
+    numer -= 1;
+    localStorage.removeItem(numer);
 }
